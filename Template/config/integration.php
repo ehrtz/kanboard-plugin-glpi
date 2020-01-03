9fdb5cd48fd1e20ede192dc76986c444cdd62363
Ferdinand Canta <ehrtz2013@gmail.com>
2020-01-03 08:35:03 +0800

    remove checkbox option for https

--- Template/config/integration.php
+++ Template/config/integration.php
@@ -1,19 +1,16 @@
 <h3><i class="fa fa-bug fa-fw"></i><?= t('GLPI Plugin') ?></h3>
 <div class="panel">
-    <?= $this->form->label(t('GLPI URL'), 'mantis_url') ?>
+    <?= $this->form->label(t('GLPI URL'), 'glpi_url') ?>
     <?= $this->form->text('glpi_url', $values) ?>
-    <?= $this->form->checkbox('glpi_scheme', t('Use Https'), 1, isset($values['glpi_scheme']) && $values['glpi_scheme'] == 1) ?>
 
-    <?= $this->form->label(t('GLPI Username'), 'mantis_username') ?>
+    <?= $this->form->label(t('GLPI Username'), 'glpi_username') ?>
     <?= $this->form->text('glpi_username', $values) ?>
 
-    <?= $this->form->label(t('GLPI password'), 'mantis_password') ?>
+    <?= $this->form->label(t('GLPI password'), 'glpi_password') ?>
     <?= $this->form->password('glpi_password', $values) ?>
 
-    <?= $this->form->label(t('GLPI User token'), 'mantis_password') ?>
+    <?= $this->form->label(t('GLPI User token'), 'glpi_password') ?>
     <?= $this->form->password('glpi_user_token', $values) ?>
-
-
 
     <div class="form-actions">
         <button type="submit" class="btn btn-blue"><?= t('Save') ?></button>
--- Template/task/import.php
+++ Template/task/import.php
@@ -2,5 +2,5 @@
     <h2><?= $this->text->e($project['name']) ?> &gt; <?= t('Add GLPI Ticket') ?></h2>
 </div>
 
-<?= $this->form->label(t('Issue ID'), 'id') ?>
+<?= $this->form->label(t('Ticket ID'), 'id') ?>
 <?= $this->form->text('id', $values, array(), array('required', 'autofocus')) ?>
